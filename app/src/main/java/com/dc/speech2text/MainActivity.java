eb13e512cb8d63206690cf2c047bbb79d4ddf959
GayathriSulam <cssgayathrisulam21@gmail.com>
2022-12-19 20:48:21 -0500

    Textview and button updated
    layout set for textview and button

--- app/src/main/java/com/dc/speech2text/MainActivity.java
+++ app/src/main/java/com/dc/speech2text/MainActivity.java
@@ -1,14 +1,33 @@
 package com.dc.speech2text;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static final String[] permissions = {Manifest.permission.RECORD_AUDIO};
+    private Button startRecordingButton;
+    private TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainHandler = new Handler();
+        isRecording = false;
+        mediaRecorder = new MediaRecorder();
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        startRecordingButton= findViewById(R.id.button);
+        displayText = findViewById(R.id.textView);
+        startRecordingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayText.setText("Start Speaking");
+            }
+        });
     }
+
 }

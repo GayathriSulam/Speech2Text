a41480a740cfb7eb7564891e611b0783ee400cc7
GayathriSulam <114685779+GayathriSulam@users.noreply.github.com>
2023-01-07 16:22:09 -0500

    Remove unused import statement

--- app/src/main/java/com/dc/speech2text/MainActivity.java
+++ app/src/main/java/com/dc/speech2text/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Handler;

0f27ef3bb540b3c218e5c90e152220f7fa772562
GayathriSulam <cssgayathrisulam21@gmail.com>
2023-01-07 13:09:33 -0500

    Final Mainactivity.java class

--- app/src/main/java/com/dc/speech2text/MainActivity.java
+++ app/src/main/java/com/dc/speech2text/MainActivity.java
@@ -1,16 +1,40 @@
 package com.dc.speech2text;
 
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_CANCEL;
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.arthenica.mobileffmpeg.Config;
+import com.arthenica.mobileffmpeg.FFmpeg;
+import com.ibm.cloud.sdk.core.http.HttpMediaType;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
+import org.json.*;
+import java.io.*;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private static final String[] permissions = {Manifest.permission.RECORD_AUDIO};
+    private boolean audioRecordingPermissionGranted,isRecording;
     private Button startRecordingButton;
     private TextView displayText;
-
+    private MediaRecorder mediaRecorder;
+    private String recordedFileName,convertedFileName;
+    private Handler mainHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +43,125 @@ public class MainActivity extends AppCompatActivity {
         isRecording = false;
         mediaRecorder = new MediaRecorder();
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-
         startRecordingButton= findViewById(R.id.button);
         displayText = findViewById(R.id.textView);
         startRecordingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                displayText.setText("Start Speaking");
+                if (!isRecording) {
+                    if (audioRecordingPermissionGranted) {
+                        try {
+                            startAudioRecording();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                convertSpeech();
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    thread.start();
+                }
             }
         });
     }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            audioRecordingPermissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        }
+        if (!audioRecordingPermissionGranted) {
+            finish();
+        }
+    }
+    private void startAudioRecording() throws IOException {
+        toggleRecording();
+        String uuid = UUID.randomUUID().toString();
+        recordedFileName = getFilesDir().getPath() + "/" + uuid + ".3gp";
+        convertedFileName = getFilesDir().getPath() + "/" + uuid + ".mp3";
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFile(recordedFileName);
+        mediaRecorder.prepare();
+        mediaRecorder.start();
+    }
+    private void convertSpeech() throws FileNotFoundException {
+        toggleRecording();
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        mediaRecorder.release();
+        int rc = FFmpeg.execute(String.format("-i %s -c:a libmp3lame %s", recordedFileName, convertedFileName));
+        if (rc == RETURN_CODE_SUCCESS) {
+            Log.i(Config.TAG, "Command execution completed successfully.");
+            try {
+                playRecording(convertedFileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            IamAuthenticator authenticator = new IamAuthenticator("bZhaj-yZMK8U9tGjhJHb3YHdC@7_a6-Qw7v6ykwfAXJm");
+            SpeechToText speechToText = new SpeechToText(authenticator);
+            speechToText.setServiceUrl("https://api.us-south.speech-to-text.watson.cloud.ibm.com");
+            File audioFile = new File(convertedFileName);
+            RecognizeOptions options = new RecognizeOptions.Builder()
+                    .audio(audioFile)
+                    .contentType(HttpMediaType.AUDIO_MP3)
+                    .model("en-AU_NarrowbandModel")
+                    .build();
+            final SpeechRecognitionResults transcript = speechToText.recognize(options).execute().getResult();
+            System.out.println(transcript);
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        JSONObject jsonObject = new JSONObject(transcript.toString());
+                        JSONArray resultsArray = jsonObject.getJSONArray("results");
+                        for (int i = 0; i < resultsArray.length(); i++) {
+                            JSONArray alternativesArray = resultsArray.getJSONObject(i).getJSONArray("alternatives");
+                            for (int j = 0; j < alternativesArray.length(); j++) {
+                                JSONObject resultObject = alternativesArray.getJSONObject(j);
+                                displayText.append("\n"+resultObject.getString("transcript")+"\n");
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } else if (rc == RETURN_CODE_CANCEL) {
+            Log.i(Config.TAG, "Command execution cancelled by user.");
+        } else {
+            Log.i(Config.TAG, String.format("Command execution failed with rc=%d and the output below.", rc));
+            Config.printLastCommandOutput(Log.INFO);
+        }
+    }
+    private void toggleRecording() {
+        isRecording = !isRecording;
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (isRecording){
+                    startRecordingButton.setText("Convert Speech");
+                }
+                else{
+                    startRecordingButton.setText("Start Speaking");
+                }
+            }
+        });
+    }
+    private void playRecording(String fileName) throws IOException {
+        MediaPlayer player = new MediaPlayer();
+        player.setDataSource(fileName);
+        player.prepare();
+        player.start();
+    }
 }
